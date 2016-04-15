c18eb224955c36016bbb0b7837d6efb432ff757f
sasmitap <pradhansasmita1991@gmail.com>
2016-04-15 11:51:03 +0530

    TestCase

--- src/com/sasmita/test/StringHelperTestCase.java
+++ src/com/sasmita/test/StringHelperTestCase.java
@@ -1,18 +1,20 @@
 package com.sasmita.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.sasmita.junit.StringHelper;
 
-public class StringHelperTest {
+
+
+public class StringHelperTestCase {
 
 	@Test
 	public void testAssertEquals(){
